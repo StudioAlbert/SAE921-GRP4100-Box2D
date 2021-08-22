f8c5ae2b77c9709bdb30f043289b300785aa54b4
Sébastien Albert <account@studio-albert.com>
2021-08-22 15:57:20 +0200

    I finally succed to make a ball falling

---
+++ 01-Gravity/include/game.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include "SFML/Window//Event.hpp"
+#include "box2d/b2_world.h"
+
+#include "ball.h"
+
+class Game{
+public:
+	Game();
+	void init();
+	void loop();
+
+	b2World& getWorld() { return world_; };
+
+	static b2Vec2 pixelsToMeters(sf::Vector2f pixels);
+	static b2Vec2 pixelsToMeters(sf::Vector2u pixels);
+	static sf::Vector2f metersToPixels(b2Vec2 meters);
+
+	static const float pixelsMetersRatio;
+
+private:
+
+	// The window ---------------------------------------------
+	sf::RenderWindow window_;
+
+	// The physical world -------------------------------------
+	b2World world_{ b2Vec2(0.0f, -9.81f) };
+
+	Ball theBall;
+
+};
